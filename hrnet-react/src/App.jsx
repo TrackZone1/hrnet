61c27396d57f25b6c623d1501328d0f1a5dcb870
TrackZone1 <48413426+TrackZone1@users.noreply.github.com>
2026-05-18 06:00:00 +0200

    style(app): finalize global styles, routing, assets and clean code

---
+++ hrnet-react/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CreateEmployee from './pages/CreateEmployee';
+import EmployeeList from './pages/EmployeeList';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CreateEmployee />} />
+        <Route path="/employee-list" element={<EmployeeList />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
